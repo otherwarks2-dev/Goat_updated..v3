ee2041e2181e1914cc0ff7dde2b3bd86e865684d
otherwarks2-dev <otherwarks2@gmail.com>
2026-08-21 07:56:11 +0600

    Update goatstore.js

--- modules/cmds/goatstore.js
+++ modules/cmds/goatstore.js
@@ -73,7 +73,7 @@ module.exports = {
     name: "goatstore",
     aliases: ["gs", "market", "cmdstore"],
     version: "0.0.6",
-    role: 2,
+    role: 1,
     author: "ArYAN",
     shortDescription: {
       en: "📌 Goatstore - Your Command Marketplace"
