7f54378eab89c0c5cd72a96a18b3d423b395473d
otherwarks2-dev <otherwarks2@gmail.com>
2026-08-21 07:38:36 +0600

    Update restart.js

--- modules/cmds/restart.js
+++ modules/cmds/restart.js
@@ -7,7 +7,7 @@ module.exports = {
     version: "1.2",
     author: "NTKhang", // updated by SaGor 
     countDown: 5,
-    role: 2,
+    role: 1,
     description: {
       vi: "Khởi động lại bot",
       en: "Restart bot"
